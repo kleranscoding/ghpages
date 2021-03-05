a4c1e84bbeb1b16c27dfe401c7641b995ead250a
kleranscoding <clarencecc88@gmail.com>
2021-03-05 15:06:59 -0800

    modified more

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './stylesheets/App.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
@@ -6,10 +6,10 @@ const baseURL = process.env.PUBLIC_URL;
 
 function Nav() {
   return (
-    <nav style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/random'>Random</Link>
+    <nav style={{ backgroundColor: "#1c1c1c", height: "75px", position: "fixed", width: "100%", marginBottom: "75px", top: 0, left: 0, display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center" }}>
+      <Link style={{color: "#ffffff"}} to='/'>Home</Link>
+      <Link style={{color: "#ffffff"}} to='/about'>About</Link>
+      <Link style={{color: "#ffffff"}} to='/random'>Random</Link>
     </nav>
   );
 }
@@ -19,7 +19,7 @@ const NotFound = () => <h1>Not Found</h1>
 function Home() {
   return (
     <>
-      <img src={baseURL + "/img/inout.JPG"} style={{ width: "480px", height: "320px" }} alt="in-out" />
+      <img src={baseURL + "/img/inout.JPG"} style={{ width: "50%", height: "50%" }} alt="in-out" />
       <p> {baseURL} </p>
       <a className="App-link" href={baseURL + "/"} target="_blank" rel="noopener noreferrer">
         React GH-pages
